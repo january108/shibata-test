41c043a732bd76303c12722175250835aaa7b9fe
Atsushi SHIBATA <shibata-atsushi@aiit.ac.jp>
2016-06-04 17:11:34 +0900

    add python file

--- test.bash
+++ test.bash
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-echo "this is a Test"
+tmp=/tmp/$$
+
+ERROR_EXIT () {
+    echo "$1" >&2
+    rm -f $tmp-*
+    exit
+}
+
+#########
+# TEST1 #
+#########
+
+echo aaa bbb NG > $tmp-ans
+./same.py aaa bbb > $tmp-out || ERROR_EXIT "TEST1-1"
+diff $tmp-ans $tmp-out || ERROR_EXIT "TEST1-2"
+
+echo OK
+rm -f $tmp-*
+exit 0
